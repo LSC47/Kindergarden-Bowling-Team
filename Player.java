af040b575635d207ebc6498abf00c74e595a70c1
Aiden Colberg <amc445@pitt.edu>
2025-01-22 12:16:51 -0500

    player feature

--- Player.java
+++ Player.java
@@ -1,3 +1,14 @@
 public class Player {
     
+    private String name = "";
+    private int score;
+
+    public Player (String n) {
+        name = n;
+        score = 0;
+    }
+
+    public void setPlayerScore(int s) {
+        score = s;
+    }
 }
