01abf2504de304b87789158b9fcc1c64a855d25c
Lucas Chough <lsc47@pitt.edu>
2025-01-27 20:54:41 -0500

    Update BowlingTeamApp.java

--- BowlingTeamApp.java
+++ BowlingTeamApp.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 
 // IMPORTANT
 // DONT RUN IN VS CODE
-// Compile with: javac -cp libs/gson-2.10.1.jar:. BowlingTeamApp.java
-// Run with: java -cp .:libs/gson-2.10.1.jar BowlingTeamApp
+// Compile with: javac -cp libs/gson-2.11.0.jar:. BowlingTeamApp.java
+// Run with: java -cp .:libs/gson-2.11.0.jar BowlingTeamApp
 // Windows (On Windows, use ; instead of : in the classpath): java -cp .;libs/gson-2.10.1.jar BowlingTeamApp
 
 public class BowlingTeamApp {
